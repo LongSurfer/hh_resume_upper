f15f96a6949c522a62f55265bef65a91f078382b
Alex Evans <ivanov2avi@yandex.ru>
2022-09-18 19:08:08 +0300

    add allure repors & python-dotenv

--- main/conftest.py
+++ main/conftest.py
@@ -1,14 +1,18 @@
-import pytest
 from selene.support.shared import browser
+import pytest
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope='function', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.browser_name = 'chrome'
-    # browser.config.base_url = 'https://hh.ru'
+    browser.config.base_url = 'https://hh.ru/'
     # browser.config.timeout = 5
-
-
 
 
 @pytest.fixture(scope='function', autouse=True)
